d2af60586d408657643dd271a2d47e6a862bb16e
ErikSalafia <erik.salafia@ativainvestimentos.com.br>
2024-05-28 23:52:11 -0300

    Desafio finalizado

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,15 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -19,6 +23,10 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+        public void DadosDoAparelho()
+        {
+            System.Console.WriteLine($"-- Dados do aparelho --\nNumero: {Numero}\nModelo: {Modelo}\nIMEI: {IMEI}\nMemoria: {Memoria}");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia= new Nokia("11 96729-7777","A1", "1A2B", 8);
+Iphone iphone= new Iphone("11 97777-7777","XR", "2A1B", 16);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("uber");
+iphone.DadosDoAparelho();
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("ifood");
+nokia.DadosDoAparelho();
